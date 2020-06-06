021b4801934f182c5a2003357df75ee3d86118ec
priyash555 <31501889+priyash555@users.noreply.github.com>
2020-06-06 16:23:00 +0530

    views added

--- home/forms.py
+++ home/forms.py
@@ -1,18 +1,13 @@
-from .models import Post
-from django import forms
+from django.forms import ModelForm
 
-class postform(forms.ModelForm):
-    
+from home.models import FileModel
+
+
+class FileForm(ModelForm):
+    """
+    Creating a form that maps to the model: https://docs.djangoproject.com/en/2.2/topics/forms/modelforms/
+    This form is used for the file upload.
+    """
     class Meta:
-        model = Post 
-        fields = ['title',
-                'content','images'
-                ]
-
-
-class commentform(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = [
-                  'content',
-                  ]
+        model = FileModel
+        fields = ['file']
--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from home import views
-from .views import starting, about
+from .views import starting, about, FileView, FileDeleteView, Predict
 
 urlpatterns = [
     path('', starting, name='home-home'),
     path('about/', starting, name='home-about'),
+    path('predict/', Predict.as_view(), name='APIpredict'),
+    path('upload/', FileView.as_view(), name='APIupload'),
+    path('delete/', FileDeleteView.as_view(), name='APIdelete'),
 ]
